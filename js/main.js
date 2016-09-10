c07d5e6972aac1b4d8a4de055f98f6b5cc90fcd6
Olivia Jack <ojackson@gmail.com>
2016-09-10 07:30:03 -0500

    stretched canvas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var canvas, smoother, glasses, video, detector, ctx, fab, drawCanvas, buttons, brush, faceCanvas, fabricCanvas, faceCtx;
+var canvas, smoother, glasses, video, detector, hand, ctx, fab, vidCanvas, vidCtx, drawCanvas, drawFab, handCanvas, handFab, buttons, brush, faceCanvas, fabricCanvas, faceCtx;
 var SCALE_HEIGHT = 60;
 
 var colors = ["#00f"];
@@ -54,17 +54,31 @@ window.onload = function() {
 		faceCanvas = document.getElementById('face');
 		faceCtx = faceCanvas.getContext('2d');
 
+		vidCanvas = document.getElementById('vidCanvas');
+		vidCtx = vidCanvas.getContext('2d');
+
 		canvas.width = window.innerWidth;
 		canvas.height = window.innerHeight;
 		ctx = canvas.getContext('2d');
 		drawCanvas = document.getElementById('fabric');
 
-		fab = new fabric.Canvas('fabric', {
+
+		drawFab = new fabric.Canvas('fabric', {
             isDrawingMode: true
         });
-        fab.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 150, left: 150, opacity: 0.7 }));
+        drawFab.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 150, left: 150, opacity: 0.7 }));
        
-        fab.renderAll();
+        drawFab.renderAll();
+
+        fab = drawFab;
+        handCanvas = document.getElementById('hand');
+
+        handFab = new fabric.Canvas('hand', {
+            isDrawingMode: true
+        });
+        
+       handFab.add(new fabric.Circle({ radius: 30, fill: '#155', top: 280, left: 150, opacity: 0.7 }));
+     handFab.renderAll();
 
 		fab.freeDrawingBrush = new fabric['PencilBrush'](fab);
       	 fab.freeDrawingBrush.width = 10;
@@ -127,7 +141,7 @@ window.onload = function() {
 	}
 
 	function drawFace(coords){
-		faceCtx.drawImage(video, coords[0], coords[1], coords[2], coords[3], 0, 0, faceCanvas.width, faceCanvas.height);
+		faceCtx.drawImage(vidCanvas, coords[0], coords[1], coords[2], coords[3], 0, 0, faceCanvas.width, faceCanvas.height);
 	}
 
 	function clearFace(coords){
@@ -137,18 +151,30 @@ window.onload = function() {
 	function play() {
 			compatibility.requestAnimationFrame(play);
 			if (video.paused) video.play();
+			
+
           	ctx.clearRect(0, 0, canvas.width, canvas.height);
+          	
+
 			if (video.readyState === video.HAVE_ENOUGH_DATA && video.videoWidth > 0) {
 	          	
-	          	// Prepare the detector once the video dimensions are known:
 	          	if (!detector) {
+	          		vidCanvas.width = video.videoWidth;
+	          		vidCanvas.height = video.videoHeight;
+	          	// Prepare the detector once the video dimensions are known:
 		      		var width = ~~(SCALE_HEIGHT * video.videoWidth / video.videoHeight); //~~removes anything to right of decimal
 					var height  =SCALE_HEIGHT;
 		      		detector = new objectdetect.detector(width, height, 1.1, objectdetect.frontalface);
+		      		hand = new objectdetect.detector(width, height, 1.1, objectdetect.handfist);
 		      	}
           		
+          		vidCtx.drawImage(video, 0, 0, vidCanvas.width, vidCanvas.height);
           		// Perform the actual detection:
 				var coords = detector.detect(video, 1);
+				var handCoords = hand.detect(video, 1);
+
+				ctx.drawImage(vidCanvas, 0, 0, canvas.width, canvas.height);
+				
 				if (coords[0]) {
 					//console.log("drawing face");
 					var coord = coords[0];
@@ -157,14 +183,30 @@ window.onload = function() {
 					ctx.strokeStyle = "rgb(200,0,0)"; // sets the color to fill in the rectangle with
 					//ctx.fillRect(10, 10, 55, 50);   // draws the rectangle at position 10, 10 with a width of 55 and a height of 50
 					
+					var canvCoord = [];
+					canvCoord[0] = coord[0]* (canvas.width / detector.canvas.width) ;
+					canvCoord[1] = coord[1]* (canvas.height / detector.canvas.height);
+					canvCoord[2] = coord[2]*(canvas.width / detector.canvas.width) ;
+					canvCoord[3] = coord[3]*(canvas.height / detector.canvas.height);
 					// Rescale coordinates from detector to video coordinate space:
-					coord[0] *= video.videoWidth / detector.canvas.width;
-					coord[1] *= video.videoHeight / detector.canvas.height;
-					coord[2] *= video.videoWidth / detector.canvas.width;
-					coord[3] *= video.videoHeight / detector.canvas.height;
+					coord[0] *= (video.videoWidth / detector.canvas.width) ;
+					coord[1] *= (video.videoHeight / detector.canvas.height) ;
+					coord[2] *= (video.videoWidth / detector.canvas.width) ;
+					coord[3] *= (video.videoHeight / detector.canvas.height) ;
+
 					
 					
-					ctx.drawImage(drawCanvas, coord[0], coord[1], coord[2], coord[3]);
+					
+
+					if(handCoords[0]){
+						console.log("hand");
+						fab = handFab;
+						ctx.drawImage(handCanvas, canvCoord[0], canvCoord[1], canvCoord[2], canvCoord[3]);
+					} else {
+						fab = drawFab;
+						ctx.drawImage(drawCanvas, canvCoord[0], canvCoord[1], canvCoord[2], canvCoord[3]);
+					}
+					
 					
 
 					
