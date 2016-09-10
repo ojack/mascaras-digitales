a0ba399f6cbeb04dd791a90d55d6c0a8eb2a3fc5
Olivia Jack <ojackson@gmail.com>
2016-09-09 22:24:27 -0500

    working version

---
+++ js/main.js
@@ -0,0 +1,118 @@
+var canvas, smoother, glasses, video, detector, ctx, fab, drawCanvas, faceCanvas, fabricCanvas, faceCtx;
+var SCALE_HEIGHT = 100;
+window.onload = function() {
+	
+		smoother = new Smoother([0.9999999, 0.9999999, 0.999, 0.999], [0, 0, 0, 0]);
+		video = document.getElementById('video');
+		glasses = document.getElementById('glasses');
+
+		getVideo();
+		createCanvas();
+		
+
+	
+		
+		
+		[].slice.call(document.getElementById('list').children).forEach(function(e) {
+			e.addEventListener('click', function() {
+				glasses.src = e.src;
+			}, false);
+		});
+	};
+
+
+
+	function getVideo(){
+		try {
+			compatibility.getUserMedia({video: true}, function(stream) {
+				try {
+					video.src = compatibility.URL.createObjectURL(stream);
+				} catch (error) {
+					video.src = stream;
+				}
+				compatibility.requestAnimationFrame(play);
+			}, function (error) {
+				alert('WebRTC not available');
+			});
+		} catch (error) {
+			alert(error);
+		}
+	}
+
+	function createCanvas(){
+		canvas = document.getElementById('c');
+		faceCanvas = document.getElementById('face');
+		faceCtx = faceCanvas.getContext('2d');
+
+		canvas.width = window.innerWidth;
+		canvas.height = window.innerHeight;
+		ctx = canvas.getContext('2d');
+		drawCanvas = document.getElementById('fabric');
+
+		fab = new fabric.Canvas('fabric', {
+            isDrawingMode: false
+        });
+        fab.add(new fabric.Circle({ radius: 30, fill: '#f55', top: 150, left: 150 }));
+        fab.renderAll();
+
+	}
+
+	function drawFace(coords){
+		faceCtx.drawImage(video, coords[0], coords[1], coords[2], coords[3], 0, 0, faceCanvas.width, faceCanvas.height);
+	}
+
+	function clearFace(coords){
+		faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
+	}
+
+	function play() {
+			compatibility.requestAnimationFrame(play);
+			if (video.paused) video.play();
+          	ctx.clearRect(0, 0, canvas.width, canvas.height);
+			if (video.readyState === video.HAVE_ENOUGH_DATA && video.videoWidth > 0) {
+	          	
+	          	// Prepare the detector once the video dimensions are known:
+	          	if (!detector) {
+		      		var width = ~~(SCALE_HEIGHT * video.videoWidth / video.videoHeight); //~~removes anything to right of decimal
+					var height  =SCALE_HEIGHT;
+		      		detector = new objectdetect.detector(width, height, 1.1, objectdetect.frontalface);
+		      	}
+          		
+          		// Perform the actual detection:
+				var coords = detector.detect(video, 1);
+				if (coords[0]) {
+					console.log("drawing face");
+					var coord = coords[0];
+					coord = smoother.smooth(coord);
+					
+					ctx.strokeStyle = "rgb(200,0,0)"; // sets the color to fill in the rectangle with
+					//ctx.fillRect(10, 10, 55, 50);   // draws the rectangle at position 10, 10 with a width of 55 and a height of 50
+					
+					// Rescale coordinates from detector to video coordinate space:
+					coord[0] *= video.videoWidth / detector.canvas.width;
+					coord[1] *= video.videoHeight / detector.canvas.height;
+					coord[2] *= video.videoWidth / detector.canvas.width;
+					coord[3] *= video.videoHeight / detector.canvas.height;
+					
+					ctx.strokeRect(coord[0], coord[1], coord[2], coord[3]);
+					ctx.drawImage(drawCanvas, coord[0], coord[1], coord[2], coord[3]);
+					// Display glasses overlay: 
+					glasses.style.left    = ~~(coord[0] + coord[2] * 1.0/8 + video.offsetLeft) + 'px';
+					glasses.style.top     = ~~(coord[1] + coord[3] * 0.8/8 + video.offsetTop) + 'px';
+					glasses.style.width   = ~~(coord[2] * 6/8) + 'px';
+					glasses.style.height  = ~~(coord[3] * 6/8) + 'px';
+
+
+					glasses.style.opacity = 1;
+
+					drawFace(coord);
+
+					
+				} else {
+					var opacity = glasses.style.opacity - 0.2;
+					glasses.style.opacity = opacity > 0 ? opacity : 0;
+
+					clearFace();
+				}
+			}
+		}
